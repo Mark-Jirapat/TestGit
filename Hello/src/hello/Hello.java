294eeb192d1b5abfa7fbcce3563a583a1a794f6f
MJS1852 <jetsteam.1852@gmail.com>
2016-03-25 16:32:52 +0700

    Change body

--- Hello/src/hello/Hello.java
+++ Hello/src/hello/Hello.java
@@ -16,7 +16,16 @@ public class Hello {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
+        for(int i = 0; i <= 5; i++){
+            switch(i){
+                case 0: System.out.println("Loading");
+                case 1: System.out.println("Hello World");   
+                case 2: System.out.println("Hello I am Korora");
+                case 3: System.out.println("I am Child of Fedora");
+                case 4: System.out.println("My Grandparents is Linux");
+                case 5: System.out.println("I have children name yum&dnf");
+            }
+        }
     }
     
 }
